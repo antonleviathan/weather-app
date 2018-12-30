494dc638c32f2edd2d33d7802864d5454f9879e6
antonleviathan <anton.livaja@gmail.com>
2018-12-30 15:01:17 -0600

    Minor refactor

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ const request = require('request');
 require('dotenv').load();
 
 const apiKey = process.env.GEOCODE_API_KEY
-console.log(apiKey);
+const addr = "1600+Amphitheatre+Parkway,+Mountain+View,+CA"
 
 request({
-  url: `https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=${apiKey}&sensor=true`,
+  url: `https://maps.googleapis.com/maps/api/geocode/json?address=${addr}&key=${apiKey}&sensor=true`,
   json: true
 }, (err, res, body) => {
   console.log(body);
